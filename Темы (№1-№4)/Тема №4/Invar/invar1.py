11c6dfea34c308a7e223d7de2be4265ce6e06b7b
KsushaSeliv <KsenijaSeliv@yandex.ru>
2019-06-27 23:27:12 +0300

    Update invar1.py

--- Темы (№1-№4)/Тема №4/Invar/invar1.py
+++ Темы (№1-№4)/Тема №4/Invar/invar1.py
@@ -2,12 +2,16 @@
 #в разных районах города. Преобразовать данные в формат csv. Разработать скрипт для визуализации данных, используя
 #библиотеку matplotlib. Для визуализации использовать тип “точечная диаграмма” (scatterplot).
 
+
 %matplotlib inline
 import numpy as np
 import math
 import pandas as pd 
 import matplotlib.pyplot as plt 
-df = pd.read_csv('ex1data1.csv', header = None) #Загружаем данные из csv-файла в скрипт с помощью метода read_csv модуля pandas.
-x, y = df[0], df[1]
+df = pd.read_csv('CENA.csv', header = None) #Загружаем данные из csv-файла в скрипт с помощью метода read_csv модуля pandas.
+l, k = df[0], df[1]
 
-plt.scatter(x, y, s = x, c ='green', label = u'Данные из файла')
+plt.scatter(l, k, c ='black')
+
+lis = list(l)
+lis2 = list(k)
